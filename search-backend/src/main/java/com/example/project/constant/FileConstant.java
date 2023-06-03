bb900aec1fb2fe4e17370f1f00472ebd77ccea09
HuianYuan <452970841@163.com>
2023-06-03 14:04:47 +0800

    后端初始化

---
+++ search-backend/src/main/java/com/example/project/constant/FileConstant.java
@@ -0,0 +1,14 @@
+package com.example.project.constant;
+
+/**
+ * 文件常量
+ *
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     * todo 需替换配置
+     */
+    String COS_HOST = " 自行修改";
+}
---
+++ search-backend/src/main/java/com/example/project/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.example.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.project.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
